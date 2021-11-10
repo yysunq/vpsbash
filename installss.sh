0f8602f03913e846782a46a617e4de9e01ddbdca
yysunq <yysunq@gmail.com>
2021-11-10 14:51:37 +0800

    Update installss.sh
    添加如果目录存在自动删除
    自动安装wget

--- installss.sh
+++ installss.sh
@@ -5,11 +5,15 @@ yum update
 yum install epel-release -y
 yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel -y
 yum install git
-
-
-
+yum install wget
 
 #安装mbedtls
+if [ -f "mbedtls-2.5.1.tar.gz" ]; then
+  rm -f mbedtls-2.5.1.tar.gz
+fi
+if [ -d "./mbedtls-mbedtls-2.5.1" ]; then
+  rm -f -r ./mbedtls-mbedtls-2.5.1
+fi
 wget https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.5.1.tar.gz --no-check-certificate
 tar -zxvf  mbedtls-2.5.1.tar.gz
 cd mbedtls-mbedtls-2.5.1
@@ -18,6 +22,12 @@ make install
 cd ..
 
 #安装libsodium
+if [ -f "libsodium-1.0.17.tar.gz" ]; then
+  rm -f libsodium-1.0.17.tar.gz
+fi
+if [ -d "./libsodium-1.0.17" ]; then
+  rm -f -r ./libsodium-1.0.17
+fi
 wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.17.tar.gz --no-check-certificate
 tar -zxvf  libsodium-1.0.17.tar.gz
 cd libsodium-1.0.17
@@ -27,6 +37,12 @@ make install
 cd ..
 
 #安装c-ares
+if [ -f "c-ares-1.13.0.tar.gz" ]; then
+  rm -f c-ares-1.13.0.tar.gz
+fi
+if [ -d "./c-ares-1.13.0" ]; then
+  rm -f -r ./c-ares-1.13.0
+fi
 wget https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz --no-check-certificate
 tar -zxvf  c-ares-1.13.0.tar.gz
 cd c-ares-1.13.0
@@ -36,6 +52,9 @@ make install
 cd ..
 
 #安装shadowsocks
+if [ -d "./shadowsocks-libev" ]; then
+  rm -f -r ./shadowsocks-libev
+fi
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
@@ -53,12 +72,16 @@ yum install crontabs
 
 #打开防火墙端口
 firewall-cmd --zone=public --add-port=8816/tcp --permanent 
+firewall-cmd --zone=public --add-port=8525/tcp --permanent 
 firewall-cmd --reload
 firewall-cmd --zone=public --list-all
 systemctl status firewalld
 #如果make出错，用下面命令configure
 #./configure --with-sodium-include=/usr/local/include --with-sodium-lib=/usr/local/lib --with-mbedtls-include=/usr/local/include --with-mbedtls-lib=/usr/local/lib
 
+if [ -d "./vpsbash" ]; then
+  rm -f -r ./vpsbash
+fi
 git clone https://github.com/yysunq/vpsbash.git
 cd vpsbash
 chmod a+x shadowsocks
