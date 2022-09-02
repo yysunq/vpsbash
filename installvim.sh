19d11ab2b80e8b4e3f57870d6a5deade52f0d383
yysunq <yysunq@gmail.com>
2022-09-02 09:55:52 +0800

    vim 自动安装配置
    vim 自动安装配置

---
+++ installvim.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Ubuntu
+
+#apt install -y vim 
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "call plug#begin('~/.vim/plugged')" >> ~/.vimrc
+echo "  Plug 'scrooloose/nerdtree'" >> ~/.vimrc
+echo "  Plug 'flazz/vim-colorschemes'" >> ~/.vimrc
+echo "  Plug 'mhinz/vim-startify'" >> ~/.vimrc
+echo "call plug#end()" >> ~/.vimrc
+echo "nnoremap <leader>n :NERDTreeFocus<CR>" >> ~/.vimrc
+echo "nnoremap <C-n> :NERDTree<CR>" >> ~/.vimrc
+echo "nnoremap <C-t> :NERDTreeToggle<CR>" >> ~/.vimrc
+echo "nnoremap <C-f> :NERDTreeFind<CR>" >> ~/.vimrc
+echo "colorscheme wombat" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
