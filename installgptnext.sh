6895c66eacee04c300c75b4cce17c5e1ef001ce7
yysunq <yysunq@gmail.com>
2023-06-29 13:53:00 +0800

    add pwd install path

--- installgptnext.sh
+++ installgptnext.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 #CentOS 7 x64
 
-#yum -y install epel-release
-#yum -y install nodejs git npm lsof
-#npm install -g yarn
+yum -y install epel-release
+yum -y install nodejs git npm lsof
+npm install -g yarn
 
 current_path=$(pwd)
-echo "current install path:$current_path"
+echo "current ChatGPT-Next-Web install path:$current_path"
 
-#git clone https://github.com/Yidadaa/ChatGPT-Next-Web
-#cd ChatGPT-Next-Web
-#echo 'export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk' >> .env.local
-#echo 'export BASE_URL=https://api.chatanywhere.cn' >> .env.local
-#echo 'export PORT=8818' >> .env.local
-#yarn install
-#source ./.env.local
-#yarn build
+git clone https://github.com/Yidadaa/ChatGPT-Next-Web
+cd ChatGPT-Next-Web
+echo 'export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk' >> .env.local
+echo 'export BASE_URL=https://api.chatanywhere.cn' >> .env.local
+echo 'export PORT=8818' >> .env.local
+yarn install
+source ./.env.local
+yarn build
 
-#cd ..
+cd ..
 if [ -d "./vpsbash" ]; then
   rm -f -r ./vpsbash
 fi
@@ -26,10 +26,10 @@ cd vpsbash
 chmod a+x run.sh
 sed -i 's|/root/gpt|'"$current_path"'|g' ./run.sh
 sed -i 's|/root/gpt|'"$current_path"'|g' ./mygpt.service
-#cp ./mygpt.service /etc/systemd/system/
-#cp ./run.sh ../ChatGPT-Next-Web
-#systemctl enable mygpt
-#systemctl start mygpt
+cp ./mygpt.service /etc/systemd/system/
+cp ./run.sh ../ChatGPT-Next-Web
+systemctl enable mygpt
+systemctl start mygpt
 
 #iptables -vL --line-number
 #iptables -I INPUT -p tcp --dport 8818 -j ACCEPT
