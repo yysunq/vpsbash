6478b316c541b73952220e5916c9bac07fab90e0
yysunq <yysunq@gmail.com>
2023-06-29 13:45:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-#!/bin/bash$
-export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk$
-export BASE_URL=https://api.chatanywhere.cn$
-export PORT=8818$
-echo $OPENAI_API_KEY$
-echo $BASE_URL$
-echo $PORT$
-cd /root/gpt/ChatGPT-Next-Web$
-yarn start$
+#!/bin/bash
+export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk
+export BASE_URL=https://api.chatanywhere.cn
+export PORT=8818
+echo $OPENAI_API_KEY
+echo $BASE_URL
+echo $PORT
+cd /root/gpt/ChatGPT-Next-Web
+yarn start
