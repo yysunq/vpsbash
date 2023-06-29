d616d1622ee285f52db504cedfb201363c6afa9f
yysunq <yysunq@gmail.com>
2023-06-29 13:24:06 +0800

    Update installgptnext.sh

--- installgptnext.sh
+++ installgptnext.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #CentOS 7 x64
 
-yum -y install epel-release
-yum -y install nodejs git npm lsof
-npm install -g yarn
+#yum -y install epel-release
+#yum -y install nodejs git npm lsof
+#npm install -g yarn
 
-git clone https://github.com/Yidadaa/ChatGPT-Next-Web
-cd ChatGPT-Next-Web
-echo 'export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk' >> .env.local
-echo 'export BASE_URL=https://api.chatanywhere.cn' >> .env.local
-echo 'export PORT=8818' >> .env.local
-yarn install
-source ./.env.local
-yarn build
+#git clone https://github.com/Yidadaa/ChatGPT-Next-Web
+#cd ChatGPT-Next-Web
+#echo 'export OPENAI_API_KEY=sk-ubAKUbHVqpiX0Z71InXghb2dCPtkjSnWM569i6Z7DX57GWCk' >> .env.local
+#echo 'export BASE_URL=https://api.chatanywhere.cn' >> .env.local
+#echo 'export PORT=8818' >> .env.local
+#yarn install
+#source ./.env.local
+#yarn build
 
-cd ..
+#cd ..
 if [ -d "./vpsbash" ]; then
   rm -f -r ./vpsbash
 fi
