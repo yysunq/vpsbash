e41d398e7c0d6380d41ee7636efc532c49af0c3f
yysunq <yysunq@gmail.com>
2023-09-08 13:00:18 +0800

    add some param

--- curl-gdriver.sh
+++ curl-gdriver.sh
@@ -2,12 +2,12 @@
 
 #name the final output file name
 #exp:filename='codegeex2-ggml.bin'
-filename='RWKV-4-Raven-7B-v12-Eng49%-Chn49%-Jpn1%-Other1%-20230530-ctx8192.pth'
+filename='RWKV-4-Novel-7B-v1-Chn-20230426-ctx8192.pth'
 
 #to get fileid, in googledriver select file to share, get copy link
 #like this https://drive.google.com/file/d/1NmmGux0pIFSEzKKkEzuxvo7hn4HfsRSI/view?usp=sharing 
 #exp:fileid='1NmmGux0pIFSEzKKkEzuxvo7hn4HfsRSI'
-fileid='1NmmGux0pIFSEzKKkEzuxvo7hn4HfsRSI'
+fileid='1-8SInoJQVeiEMkNtLC0bREZO9VGwDzG5'
 
 #to get accesstoken
 #https://developers.google.com/oauthplayground/ => 
@@ -16,10 +16,10 @@ fileid='1NmmGux0pIFSEzKKkEzuxvo7hn4HfsRSI'
 #Step1 push button "Authorize APIS" =>
 #Step2 push button "Exchange authorization code for tokens" to generate Access token
 #exp:accesstoken='ya29.a0AfB_byCs7JXNtyw01G9zoi0QXELxukOBjwTUmpeht_G28DKoA69Vcac7c4Zji-0kj0VLVD0C0Ol33pTabpTT49_gs_D8qNbqtP5WhBw1VFz-E4ME5U75fTI_ZJdMKKyTZiM7WkS_HbArYAlf7Aeb_pYydLfYyc0bERdL0QaCgYKAf0SARMSFQHsvYlsO4fPAyFXh6daxcowaAUn4w0173'
-accesstoken='ya29.a0AfB_byCs7JXNtyw01G9zoi0QXELxukOBjwTUmpeht_G28DKoA69Vcac7c4Zji-0kj0VLVD0C0Ol33pTabpTT49_gs_D8qNbqtP5WhBw1VFz-E4ME5U75fTI_ZJdMKKyTZiM7WkS_HbArYAlf7Aeb_pYydLfYyc0bERdL0QaCgYKAf0SARMSFQHsvYlsO4fPAyFXh6daxcowaAUn4w0173'
+accesstoken='ya29.a0AfB_byDGWh4iZFfjosUTtlL2-eaAq2owBnk0wgXiKd4v9GXq-TehxrRp5Y0kAgRbE-OXmRF29J9UBvu9g2O4FbvGR8mvsQruMu6pz7yiiemNmu9a-kc_kDOmC2VPi2VHnC4x1nACVQ7SGe-oCO47z1X_SHCPvNX8a5oeRQaCgYKAfUSARMSFQHsvYlslE7Exu0TJOElpl5ZlQKNGw0173'
 
 #run!!!!
-curl -H "Authorization: Bearer ${accesstoken}" https://www.googleapis.com/drive/v3/files/${fileid}=media -o ${filename}
+echo curl -H \"Authorization: Bearer ${accesstoken}\" https://www.googleapis.com/drive/v3/files/${fileid}?alt=media -o ${filename}
 
 
 
